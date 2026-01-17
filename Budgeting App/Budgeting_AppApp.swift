f0ab75cfdbfccc67b5d46534271cb839cba7cd2a
Tim Wong <timothywong930@gmail.com>
2026-01-16 21:11:46 -0800

    Initial Commit

---
+++ Budgeting App/Budgeting_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Budgeting_AppApp.swift
+//  Budgeting App
+//
+//  Created by Timothy Wong on 1/16/26.
+//
+
+import SwiftUI
+
+@main
+struct Budgeting_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Budgeting App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Budgeting App
+//
+//  Created by Timothy Wong on 1/16/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
